d91f255e097b37afe8b22dacbf07f4984b75da33
HemanthSikhakolanu <231fa04934@gmail.com>
2026-08-19 12:06:27 +0530

    Model version 2 - Random Forest 200 trees

--- src/train.py
+++ src/train.py
@@ -22,8 +22,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = RandomForestClassifier(
-    n_estimators=100,
-    max_depth=5,
+    n_estimators=200,
+    max_depth=10,
     random_state=42
 )
 
